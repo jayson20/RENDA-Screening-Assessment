cdfcb356c66efce5e44913e7caa0d528082020e2
Oluwashogo Johnson Abolarin <ojabolarin@sidmach.com>
2023-03-10 12:25:00 +0100

    Question 1

--- RENDA-Test-Exercise/Program.cs
+++ RENDA-Test-Exercise/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+// Declare an array of integer type
+int[] data = { 3, 4, 3, 6, 2, 3, 4, 5, 7, 3 };
+Console.WriteLine("Array before removing duplicate values: ");
+Array.ForEach(data, i => Console.WriteLine(i));
+
+//remove duplicate integers
+int[] unique = data.Distinct().ToArray();
+
+// Display the final result
+Console.WriteLine("Array after removing duplicate values: ");
+Array.ForEach(unique, j => Console.WriteLine(j));
+
+
+
+
+
+
+
+
